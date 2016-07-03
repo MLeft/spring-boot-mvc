12a97e736a0fd0e32ab21940d807220fab5fad9d
hugh_from_home <1015806827@qq.com>
2016-07-04 00:09:40 +0800

    解决mybatis generator生成的文件中文可能会乱码

--- src/main/java/demo/hugh/mvc/mbg/MyCommentGenerator.java
+++ src/main/java/demo/hugh/mvc/mbg/MyCommentGenerator.java
@@ -7,6 +7,9 @@ import org.mybatis.generator.internal.DefaultCommentGenerator;
 
 public class MyCommentGenerator extends DefaultCommentGenerator {
 
+	// mybatis generator生成的文件可能会乱码，通过在Eclipse_Home/eclipse.ini文件中，指定 -Dfile.encoding=UTF-8
+	// 因为mybatis generator生成文件时，使用的默认字符集是在java虚拟机启动时决定的，依赖于java虚拟机所在的操作系统的区域以及字符集
+	// 在java.nio.charset包中的Charset.java中，默认字符集就是从file.encoding这个属性中获取的。
 	@Override
 	public void addFieldComment(Field field,
 			IntrospectedTable introspectedTable,
